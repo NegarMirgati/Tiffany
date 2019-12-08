084e8b8785251e1a90f3c10ffe1232bd78b499fe
Negar <negar.nbj96@yahoo.com>
2019-12-08 14:15:48 +0330

    resolve conflicts

--- GoodreadsChoiceAwards/spiders/NomineesSpider.py
+++ GoodreadsChoiceAwards/spiders/NomineesSpider.py
@@ -8,8 +8,6 @@ from GoodreadsChoiceAwards.items import nomineeItem
 class NomineeSpider(scrapy.Spider):
     name = "nominees"
     start_urls = [
-        #'https://www.goodreads.com/choiceawards/best-books-2009',
-        #'https://www.goodreads.com/choiceawards/best-books-2010',
         #'https://www.goodreads.com/choiceawards/best-books-2011',
         #'https://www.goodreads.com/choiceawards/best-books-2012',
         #'https://www.goodreads.com/choiceawards/best-books-2013',
@@ -18,14 +16,12 @@ class NomineeSpider(scrapy.Spider):
         #'https://www.goodreads.com/choiceawards/best-books-2016',
         #'https://www.goodreads.com/choiceawards/best-books-2017'
         #'https://www.goodreads.com/choiceawards/best-books-2018'
-        'https://www.goodreads.com/choiceawards/best-books-2019'   
+        #'https://www.goodreads.com/choiceawards/best-books-2019'   
     ]
 
     def parse(self, response):
-       
         u = urlparse(response.request.url).path
         award = os.path.split(u)[1]
-        #//*[@id="categories"]/div/div[1]
         for div in response.xpath('.//*[@id="categories"]/div[@class="categoryContainer"]/div[@class="category clearFix"]'):
             category = div.xpath('.//a/h4/text()').extract_first().rstrip().replace('\n', '')
             url = urljoin(response.url, div.xpath('.//a/@href').extract_first())
@@ -36,7 +32,7 @@ class NomineeSpider(scrapy.Spider):
         print('Parse function called on %s', response.url)
         title = response.xpath('.//title/text()').extract_first()
         book = nomineeItem()
-        for div in response.xpath('.//*[@class="pollContents"]/div[@class="inlineblock pollAnswer"]'):
+        for div in response.xpath('.//*[@class="pollContents"]/div[@class="inlineblock pollAnswer resultShown pollAnswer--last"]'):
             url = div.xpath('.//a[@class="pollAnswer__bookLink"]/@href').extract_first()
             name = div.xpath('.//a[@class="pollAnswer__bookLink"]/img/@alt').extract_first()
             id = div.xpath('.//*[@id="book_id"]/@value').extract_first()
